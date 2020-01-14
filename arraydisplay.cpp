2bdfa4d7d0f8f4994ec6990727ec06a1aab86317
Aditya Dube <32643633+errordube@users.noreply.github.com>
2020-01-14 22:27:49 +0530

    Add files via upload

---
+++ arraydisplay.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n; //Size of the array
+cout<<"Enter the size of the array\n";
+cin>>n; //input size of the array
+int a[n];
+cout<<"Enter the values\n"; //Entering the values in an array
+for(int i=0;i<n;i++)
+{
+
+cin>>a[i]; //Taking input
+
+}
+cout<<"The values entered are\n"; //Displaying the array elements
+
+for(int i=0;i<n;i++)
+
+{
+
+cout<<a[i]<<endl;
+
+}
+return 0;
+
+}
